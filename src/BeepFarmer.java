513a318fd34fae42393f3e66a7f4b9cb319e9fc8
JordanKoski <jordan_koski@asl.oeg>
2014-12-16 09:43:17 +0000

    3 classes and Test Challenge 1

--- src/BeepFarmer.java
+++ src/BeepFarmer.java
@@ -1,35 +1,35 @@
 import org.asl.karelx.Farmer;
 
-/**
- * Represents a farmer capable of sowing or reaping beepers from a rectangular garden
- *
- */
+
 public class BeepFarmer extends Farmer {
 
-	//  TODO Add one or more constructors, as needed
+	 public BeepFarmer()
+      {
+         super (x, y, Display.NORTH);
+      }
+
+	public void reap(int width, int height) 
+   {
+		putBeeper();
+      if (frontIsClear());
+      {
+      move();
+      putBeeper();
+      }
+      else if (!frontIsClear && !leftIsClear());
+      {
+      turnRight();
+      move();
+      turnRight();
+      putBeeper();
+      }
+      else if (!frontIsClear && !rightIsClear());
+      turnLeft();
+      move();
+      turnLeft();
+      putBeeper();
+	   }
 	
-	/**
-	 * Access all locations in a rectangular garden and pick up any beepers found
-	 * 
-	 * Precondition: Farmer is starting in the southwest corner of the area to be harvested.
-	 * 
-	 * @param width			the width of the garden (number of columns)
-	 * @param height		the height of the garden (number of rows)
-	 * 
-	 * Ex. reap(7, 4) should gather all beepers from an area 7 rows wide and 4 rows tall
-	 */
-	public void reap(int width, int height) {
-		// TODO You implement this
-	}
-	
-	/**
-	 * Access all locations in a rectangular garden and plant one beeper at each location
-	 * 
-	 * Precondition: Farmer is starting in the southwest corner of the area to be sown.
-	 * 
-	 * @param width		the width of the garden (number of columns)
-	 * @param height	the height of the garden (number of rows)
-	 */
 	public void sow(int width, int height) {
 		int startX = this.getX();
 		int startY = this.getY();
--- src/TestChallenge1.java
+++ src/TestChallenge1.java
@@ -1,15 +1,38 @@
-/**
- * Read and implement all of the TODO items in the main method, below.
- * 
- */
-public class TestChallenge1 {
-	// TODO Complete the provided Sentry class by implementing its patrol() method
+import java.lang.Object;
+import edu.fcps.karel2.Item;
+import edu.fcps.karel2.Robot;
+import org.asl.karelx.Uberbot;
+import org.asl.karelx.Squarebot;
 
+public class TestChallenge1 {
+	
 	public static void main(String[] args) {
-		// TODO Load a default map 
-		// TODO Create a Squarebot and have it place beepers at the corners of a rectangle
-		//   	with bottom right corner at (9, 3) and sized for 5 beepers on each side (see picture)
-		// TODO Create a Sentry and have it patrol clockwise around the beepers, indefinitely
+		 Display.openWorld();
+		 Display.setSize(10, 10);
+		 Uberbot jordan = new Squarebot(9, 3, Display.NORTH, INFINITY);
+       Uberbot lisa = new Sentry (9, 3);
+		 
+       jordan.putBeeper();
+       jordan.move(5);
+       jordan.putBeeper();
+       jordan.turnLeft();
+       
+       jordan.putBeeper();
+       jordan.move(5);
+       jordan.putBeeper();
+       jordan.turnLeft();
+       
+       jordan.putBeeper();
+       jordan.move(5);
+       jordan.putBeeper();
+       jordan.turnLeft();
+       
+       jordan.putBeeper();
+       jordan.move(5);
+       jordan.putBeeper();
+       jordan.turnLeft();
+       
+       lisa.patrol();
 	}
 
 }
